197671cf8e12b0d79a7f556cd1e863eab02b8165
荣顶 <wangrongding@qq.com>
2024-03-21 02:02:24 +0800

    chore: 修改 about 文案

--- src/views/page-about.vue
+++ src/views/page-about.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="about">
-    <h1>video-studio</h1>
+  <div class="about p-10">
+    <h1 class="mb-5 text-3xl font-bold">video-studio</h1>
     <p>这是一个基于 web 端的视频编辑器，正在开发中。</p>
+    <RouterLink to="/">编辑器</RouterLink>
   </div>
 </template>
 
