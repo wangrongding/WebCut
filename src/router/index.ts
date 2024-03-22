1a689c55d6ab95ff1cf3a35ce623a9c1def5294a
荣顶 <wangrongding@qq.com>
2024-03-23 00:49:16 +0800

    chore: 调整文件名

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import main from '../views/page-main.vue'
+import main from '../views/main-page.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +20,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/page-about.vue')
+      component: () => import('../views/about-page.vue')
     }
   ]
 })
