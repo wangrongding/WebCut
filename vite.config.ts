6a7abbe9746b2de342cb17b813d1d539e2e65781
荣顶 <wangrongding@qq.com>
2024-03-23 00:40:22 +0800

    chore: 修改别名标识

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,10 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '~': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
