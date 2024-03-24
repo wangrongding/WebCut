53f5de5b1884ffe3107599cc8353196ae90cde2c
荣顶 <wangrongding@qq.com>
2024-03-24 23:11:35 +0800

    chore: 修改类型文件路径

---
+++ types/mp4box.d.ts
@@ -0,0 +1,5 @@
+// https://github.com/gpac/mp4box.js/issues/233
+// https://github.com/kixelated/moq-js/blob/9a8053db72eaabe77d6d325a7de9375c6c0d4719/types/mp4box.d.ts
+
+// TODO mp4box.js 目前没有提供 typescript 类型定义文件，后续需要完善，或更换其他库
+declare module 'mp4box'
