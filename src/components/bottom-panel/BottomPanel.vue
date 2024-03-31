319c4c5a70d4dc0699613311b9991b3a82a9c76a
荣顶 <wangrongding@qq.com>
2024-03-31 17:10:02 +0800

    feat: 完善底部面板样式

--- src/components/bottom-panel/BottomPanel.vue
+++ src/components/bottom-panel/BottomPanel.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
+import TrackList from './TrackList.vue'
 import mitt from 'mitt'
+import { storeToRefs } from 'pinia'
 import IconCopy from '~/assets/icons/control-copy.svg?component'
 import IconCut from '~/assets/icons/control-cut.svg?component'
 import IconDelete from '~/assets/icons/control-delete.svg?component'
 import IconNext from '~/assets/icons/control-next.svg?component'
 import IconPause from '~/assets/icons/control-pause.svg?component'
+import IconPlay from '~/assets/icons/control-play.svg?component'
 import IconPrev from '~/assets/icons/control-prev.svg?component'
 import IconRedo from '~/assets/icons/control-redo.svg?component'
 import IconUndo from '~/assets/icons/control-undo.svg?component'
 import IconZoomin from '~/assets/icons/control-zoomin.svg?component'
 import IconZoomout from '~/assets/icons/control-zoomout.svg?component'
 import IconZoomToFit from '~/assets/icons/control-zoomtofit.svg?component'
+import { usePlayerStore } from '~/stores/player'
+
+const playerStore = usePlayerStore()
+// 将 store 中的 playStatus 转换为 ref
+const { playStatus } = storeToRefs(playerStore)
 
 const emitter = mitt()
 function toggleVideoPlay() {
-  emitter.emit('player:toggle-play')
+  playerStore.playStatus = !playStatus.value
+  // emitter.emit('player:toggle-play')
 }
 </script>
 <template>
@@ -22,27 +31,49 @@ function toggleVideoPlay() {
     <!-- 刻度尺 -->
     <div class="flex border-y justify-between border-[#272836] h-[40px] items-center gap-2">
       <div class="flex gap-2">
-        <button><IconUndo /></button>
-        <button><IconRedo /></button>
-        <button><IconCut /></button>
-        <button><IconDelete /></button>
-        <button><IconCopy /></button>
+        <button class="tooltip btn-control" data-tip="撤销"><IconUndo /></button>
+        <button class="tooltip btn-control" data-tip="恢复"><IconRedo /></button>
+        <button class="tooltip btn-control" data-tip="分割"><IconCut /></button>
+        <button class="tooltip btn-control" data-tip="删除"><IconDelete /></button>
+        <button class="tooltip btn-control" data-tip="复制"><IconCopy /></button>
       </div>
       <div class="flex items-center gap-2">
-        <button><IconPrev /></button>
-        <button><IconPause /></button>
-        <button><IconNext /></button>
+        <button class="btn-control"><IconPrev /></button>
+        <button class="btn-control" v-if="playStatus" @click="toggleVideoPlay">
+          <IconPlay />
+        </button>
+        <button class="btn-control" v-else @click="toggleVideoPlay">
+          <IconPause />
+        </button>
+        <button class="btn-control"><IconNext /></button>
         <span>00:00:00 / 00:00:00</span>
       </div>
       <div class="flex gap-4">
-        <button><IconZoomout /></button>
-        <button><IconZoomin /></button>
-        <button><IconZoomToFit /></button>
+        <button class="tooltip btn-control" data-tip="放大"><IconZoomout /></button>
+        <div class="flex items-center gap-2">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value="40"
+            class="range range-xs [--range-shdw:#00a7ea]"
+          />
+          {{ '40%' }}
+        </div>
+        <button class="tooltip btn-control" data-tip="缩小"><IconZoomin /></button>
+        <button class="tooltip" data-tip="适配到合适的大小">
+          <IconZoomToFit class="btn-control" />
+        </button>
         <button>预览</button>
       </div>
     </div>
 
-    <div class="grid content-center flex-1 text-center">bottom-panel ( 开发中... )</div>
+    <!-- 轨道列表 -->
+    <TrackList />
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style scoped>
+.btn-control {
+  @apply rounded-sm hover:bg-[#ee2788];
+}
+</style>
---
+++ src/components/bottom-panel/TrackList.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import IconMusic from '~/assets/icons/icon-music.svg?component'
+import IconText from '~/assets/icons/icon-text.svg?component'
+import IconVideo from '~/assets/icons/icon-video.svg?component'
+</script>
+<template>
+  <div class="grid content-center flex-1 text-center">
+    <div class="element-track">
+      <div class="flex gap-2">
+        <IconText />
+        <span>+ 添加文字等素材</span>
+      </div>
+    </div>
+    <div class="video-track">
+      <div class="flex gap-2">
+        <IconVideo />
+        <span>+ 视频轨道</span>
+      </div>
+    </div>
+    <div class="music-track">
+      <div class="flex gap-2">
+        <IconMusic />
+        <span>+ 添加音频</span>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.music-track,
+.video-track,
+.element-track {
+  width: 100%;
+  padding-left: 10px;
+  font-size: 0.875rem;
+  margin: 4px 0px;
+  display: flex;
+  align-items: center;
+  border: 2px dashed rgb(59, 59, 79);
+  color: rgb(110, 110, 134);
+  border-radius: 5px;
+  cursor: pointer;
+  height: 28px;
+}
+.video-track {
+  // justify-items: flex-start;
+  // align-content: center;
+  transition: width 200ms ease-out 0s;
+  position: relative;
+  height: 48px;
+  background: rgb(30, 30, 41);
+}
+.music-track {
+  background: url('~/assets/imgs/music-ripples.png') left center / auto 100% repeat-x;
+}
+</style>
